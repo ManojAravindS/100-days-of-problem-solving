742174b58a4d9ff5d0b33bbc8eb21356c2f43bbc
Manoj Aravind S <manojaravind1511@gmail.com>
2026-01-19 19:12:29 +0530

    Add files via upload

---
+++ 2. Add Two Numbers/AddTwoNumbers.java
@@ -0,0 +1,24 @@
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        int carry = 0; int total = 0;
+        ListNode temp = new ListNode(0);
+        ListNode dummy = temp;
+        while(l1 != null || l2 != null || carry != 0){
+            int v1 = 0, v2 = 0;
+            if(l1 != null){
+                v1 = l1.val;
+                l1 = l1.next;
+            }
+	    if(l2 != null){
+	        v2 = l2.val;
+		l2 = l2.next;
+	    }
+	    total = v1 + v2 + carry;
+	    carry = total/10;
+	    total = total%10;
+	    temp.next = new ListNode(total);
+	    temp = temp.next;
+        }
+        return dummy.next;
+    }
+}
