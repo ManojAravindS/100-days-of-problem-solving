c734eab615c967c74ed245aa1eb276f5d930c850
Manoj Aravind S <manojaravind1511@gmail.com>
2026-01-30 22:24:28 +0530

    Add files via upload

---
+++ 118. Pascal's Triangle/pascalsTriangle.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> first = new ArrayList<>();
+        first.add(1);
+        res.add(first);
+
+        for(int i = 1; i < numRows; i++){
+            List<Integer> prev = res.get(i-1);
+            List<Integer> curr = new ArrayList<>();
+
+            curr.add(1);
+            for(int j = 1; j < i; j++){
+                curr.add(prev.get(j)+prev.get(j-1));
+            }
+            curr.add(1);
+            res.add(curr);
+        }
+        return res;
+    }
+}
